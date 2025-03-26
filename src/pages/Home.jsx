5876e91b00db7b39412830f65000da3ef9fb6cad
Lasarikoo <lazaro@bega.ltd>
2025-03-26 19:24:21 +0000

    First API data received

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,10 @@
-import rigoImageUrl from "../assets/img/rigo-baby.jpg";
-import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import People from "./People.jsx";
 
 export const Home = () => {
 
-  const {store, dispatch} =useGlobalReducer()
-
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
+			<People />
 		</div>
 	);
 }; 
